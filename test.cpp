99001f78aede8f2afb75c2d273fe4013264c93a6
Klu <kevinmallari62@gmail.com>
2022-03-07 18:13:25 +0800

    Converts string vector to chars* vector.

--- test.cpp
+++ test.cpp
@@ -1,24 +1,49 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #include <iostream>
+
+// ADDITIONAL LIBRARIES NEEDED
+#include <stdio.h>
+#include <string.h>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 using namespace std;
 
-// split string using delimiter
-vector<string> split(string str, char delimiter) {
+// split character array using delimiter
+vector<char *> split(string str, char delimiter)
+{
   vector<string> internal;
-  stringstream ss(str); // Turn the string into a stream.
+  stringstream ss(str);  // Turn the string into a stream.
   string tok;
-  while(getline(ss, tok, delimiter)) {
+
+  while (getline(ss, tok, delimiter)) {
     internal.push_back(tok);
   }
-  return internal;
+
+  // translate vector of strings to vector of characters
+  vector<char *> chars;
+
+  for (int i = 0; i < internal.size(); i++)
+  {
+    char *c = new char[internal[i].length() + 1];
+    strcpy(c, internal[i].c_str());
+    chars.push_back(c);
+  }
+
+  return chars;
 }
 
-int main () {
-  string a = "mkdir folder_name";
-  vector<string> b = split(a, ' ');
-  for (int i = 0; i < b.size(); i++) {
-    cout << b[i] << endl;
-  }
+int main()
+{
+  string a = "ls -l";
+  vector<char *> b = split(a, ' ');
+  // for (int i = 0; i < b.size(); i++)
+  // {
+  //   cout << b[i] << endl;
+  // }
+
+  execvp("ls", b.data());
 }
