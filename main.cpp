6fe1d8906a810d78b43a4367f65c7c57368117d4
Decaffeinated-Usagi <luke.lao@obf.ateneo.edu>
2022-03-07 17:49:57 +0800

    fail

--- main.cpp
+++ main.cpp
@@ -1,26 +1,58 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h> //https://www.geeksforgeeks.org/wait-system-
-
-#include <unistd.h>
+#include <sys/wait.h> 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-#define MAX LINE 80 /* The maximum length command */
-int main(void)
+void command_parse(string args, string &parseCommand, vector<string> &parseParam)
 {
-char *args[MAX LINE/2 + 1]; /* command line arguments */
-int should run = 1; /* flag to determine when to exit program */
-while (should run) {
-cout << "osh>";
-/**
-* After reading user input, the steps are:
-* (1) fork a child process using fork()
-* (2) the child process will invoke execvp()
-* (3) parent will invoke wait() unless command included &
-*/
+  stringstream sstream(args);
+  string temp;
+
+  vector<string> out;
+  
+  while (getline(sstream, temp, ' ')) 
+  {
+    parseParam.push_back(temp);
+  }
+
+  parseCommand = parseParam[0];
+  parseParam.erase(parseParam.begin());
 }
-return 0;
+
+
+int main()
+{
+  string args; /* command line arguments */
+  string command; //command
+  vector<string> params;
+  int should_run = 1; /* flag to determine when to exit program */
+  while (should_run) {
+      cout << "osh>";
+
+      getline(cin, args);
+
+      command_parse(args, command, params);
+
+      cout << "INPUT: " << command << "   ";
+      
+      for (int i = 0; i<params.size(); i++)
+      {
+          cout << params[i];
+      }
+
+      cout <<endl;
+
+
+    /**
+    * After reading user input, the steps are:
+    * (1) fork a child process using fork()
+    * (2) the child process will invoke execvp()
+    * (3) parent will invoke wait() unless command included &
+    */
+    }
+  return 0;
 }
