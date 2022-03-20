e68374f6d2c0b2f6f44671e371eb5f7115fd7341
Decaffeinated-Usagi <luke.lao@obf.ateneo.edu>
2022-03-20 21:42:31 +0800

    final edits

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+//Project 1 Rudimentary Shell Interpreter
+//ENGG 126 A
+//Coded by:
+//Felipe Gan
+//Kevin Mallari
+//Luke Lao
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -257,19 +264,22 @@ void checkInput(string command)
   size_t redirout = command.find('>');
   size_t pipe = command.find('|');
 
+  //redirection in
   if (redirin != string::npos)
   {
     execCommand(command, 2);
   }
+  //redirection out
   else if (redirout != string::npos)
   {
     execCommand(command, 1);
   }
-
+  //pipe
   if (pipe != string::npos)
   {
     execCommand(command, 3);
   }
+  //regular command
   else if (redirin == string::npos && redirout == string::npos)
   {
     execCommand(command, 0);
